9a019a637c85e8a972aeb96bad51804648ec973c
gabriel-05-dev <g.luchita@protonmail.com>
2024-06-21 14:33:00 +0100

    added dialog when user presses add new book.

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 const userLibrary = [];
+const libraryElement = document.querySelector('.library');
+
+const addBooksBtn = document.querySelector('#addBook');
+const dialog = document.querySelector('#dialog');
+const closeDialog = document.querySelector('#closeDialog');
+
 
 function newBook(title, author, pages, read) {
     this.title = title;
@@ -10,3 +16,24 @@ function newBook(title, author, pages, read) {
 function appendBook(book) {
     userLibrary.push(book);
 };
+
+/* Open/close the dialog */
+
+addBooksBtn.addEventListener('click', ()=> {
+    dialog.showModal();
+});
+
+closeDialog.addEventListener('click', ()=> {
+    dialog.close();
+});
+
+
+/*
+
+addBooksBtn.addEventListener('click', function() {
+    const NewCard = document.createElement('div');
+    NewCard.classList.add("card");
+    libraryElement.appendChild(NewCard);
+});
+
+*/
