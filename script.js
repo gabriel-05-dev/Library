d3431d7f1ac3111c4b3b16d5440fc55e2034a80d
gabriel-05-dev <g.luchita@protonmail.com>
2024-06-22 13:31:34 +0100

    added function to store books in cards, modified styling for cards, made form reset on submission

--- script.js
+++ script.js
@@ -12,19 +12,28 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const obj = Object.fromEntries(formData);
-
+    /* move obj data into variables. */
     let book = obj.bookName;
     let author = obj.author;
     let pages = obj.pages;
-    console.log(book);
-    console.log(author);
-    console.log(pages);
-    return book, author, pages;
+    createCard(book, author, pages);
+    form.reset();
 });
 
 function createCard(book, author, pages) {
     const NewCard = document.createElement('div');
+    const BookTitle = document.createElement('h3');
+    const BookAuthor = document.createElement('p');
+    const BookPages = document.createElement('p');
+    BookTitle.innerText = book;
+    BookAuthor.innerText = 'Author : ' + author;
+    BookPages.innerText = 'Pages : ' + pages;
+    
+
     NewCard.classList.add("card");
+    NewCard.appendChild(BookTitle);
+    NewCard.appendChild(BookAuthor);
+    NewCard.appendChild(BookPages);
     libraryElement.appendChild(NewCard);
 };
 
