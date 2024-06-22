6125370d137a845520950ad45d385455081a3935
gabriel-05-dev <g.luchita@protonmail.com>
2024-06-22 18:18:03 +0100

    added function to modify read/not read status on book objects.

--- script.js
+++ script.js
@@ -14,9 +14,17 @@ form.addEventListener('submit', (e) => {
     e.preventDefault();
     const formData = new FormData(form);
     const bookObj = Object.fromEntries(formData);
-    /* move obj data into variables. */
+
+    //ensure book object has a read status.
+    if (!bookObj.read) {
+        bookObj.read = false;
+    } else if (bookObj.read == 'on') {
+        bookObj.read = true;
+    };
+
     user_library.push(bookObj);
-    create_card();
+    console.log(bookObj);
+    create_cards();
     form.reset();
 });
 
@@ -37,34 +45,49 @@ clear_books_btn.addEventListener('click', function() {
 });
 
 function remove_from_library(id) {
+    console.log(id);
     if (id == 0) {
         user_library.shift(id);
-        create_card();
-    } else {
-        user_library.splice(id,id);
-        create_card();
+        create_cards();
+    } else if (id > 0) {
+        user_library.splice(id, 1);
+        create_cards();
     };
+};
 
-}
+function update_read(i) {
+    if (user_library[i].read === false) {
+        return user_library[i].read = true;
+    } else if (user_library[i].read === true) {
+        return user_library[i].read = false;
+    };
+};
 
-function create_card() {
+function create_cards() {
 
     library_element.innerHTML = '';
 
     for (let i = 0; i<user_library.length; i++) {
-        //* for each element in the array, create a card & push it onto the library*/
+        /* for each element in the array, create a card & push it onto the library*/
         user_library[i].id = i;
-        console.log(user_library[i], user_library.id)
         const new_card = document.createElement('div');
         const book_title = document.createElement('h3');
         const book_author = document.createElement('p');
         const book_pages = document.createElement('p');
         const remove_book = document.createElement('button');
         const read_book_div = document.createElement('div');
+        const read_book_status = document.createElement('div');
         const read_book_btn = document.createElement('button');
+
+        //event listeners for the cards.
+
         remove_book.addEventListener('click', function() {
             remove_from_library(i);
         });
+        read_book_div.addEventListener('click', function() {
+            update_read(i);
+        });
+
         remove_book.innerText = 'remove?';
         read_book_btn.innerText = 'read?';
         read_book_div.appendChild(read_book_btn);
