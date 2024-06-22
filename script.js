ed18ab3a0e427a47afc7e1c01928ca0c6bed494e
gabriel-05-dev <g.luchita@protonmail.com>
2024-06-22 15:08:52 +0100

    added display/hide cards function w/ counter

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const add_books_btn = document.querySelector('#addBook');
 const dialog = document.querySelector('#dialog');
 const close_dialog = document.querySelector('#closeDialog');
 const form = document.getElementById('form');
+const display_btn = document.querySelector('#displayLibrary');
+let display_counter = 0;
 
 
 form.addEventListener('submit', (e) => {
@@ -16,28 +18,6 @@ form.addEventListener('submit', (e) => {
     form.reset();
 });
 
-function create_card() {
-
-    library_element.innerHTML = '';
-
-    for (let i = 0; i<userLibrary.length; i++) {
-
-        //* for each element in the array, create a card & push it onto the library*/
-        const new_card = document.createElement('div');
-        const book_title = document.createElement('h3');
-        const book_author = document.createElement('p');
-        const BookPages = document.createElement('p');
-        book_title.innerText = userLibrary[i].bookName;
-        book_author.innerText = userLibrary[i].author;
-        BookPages.innerText = userLibrary[i].pages;
-        new_card.classList.add("card");
-        new_card.appendChild(book_title);
-        new_card.appendChild(book_author);
-        new_card.appendChild(BookPages);
-        library_element.appendChild(new_card);
-        };
-    };
-
 add_books_btn.addEventListener('click', ()=> {
     dialog.showModal();
 });
@@ -45,3 +25,49 @@ add_books_btn.addEventListener('click', ()=> {
 close_dialog.addEventListener('click', ()=> {
     dialog.close();
 });
+
+display_btn.addEventListener('click', function() {
+    display_cards();
+});
+
+function create_card() {
+
+    library_element.innerHTML = '';
+
+    for (let i = 0; i<user_library.length; i++) {
+
+        //* for each element in the array, create a card & push it onto the library*/
+        const new_card = document.createElement('div');
+        const book_title = document.createElement('h3');
+        const book_author = document.createElement('p');
+        const book_pages = document.createElement('p');
+
+        const read_book_div = document.createElement('div');
+        const read_book_checkbox = document.createElement('input');
+        const read_book_text = document.createElement('p');
+        read_book_checkbox.type = 'checkbox';
+        read_book_text.innerText = 'Read?'
+        read_book_div.appendChild(read_book_text);
+        read_book_div.appendChild(read_book_checkbox);
+        read_book_div.classList.add('readStatus');
+
+        book_title.innerText = user_library[i].bookName;
+        book_author.innerText = 'Author: '+user_library[i].author;
+        book_pages.innerText = 'Pages: '+user_library[i].pages;
+        new_card.classList.add("card");
+        new_card.appendChild(book_title);
+        new_card.appendChild(book_author);
+        new_card.appendChild(book_pages);
+        new_card.appendChild(read_book_div);
+        library_element.appendChild(new_card);
+        };
+    };
+
+function display_cards() {
+    if (display_counter % 2 != 0) {
+        library_element.style = 'display:none;';
+        display_counter++
+    } else {
+        library_element.style = '';
+        display_counter++
+    }};
